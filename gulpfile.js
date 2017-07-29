37f453254243b74d85463846205ca6a18d4a2664
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2017-07-29 08:14:35 +0200

    added gulp coffee task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util')
+    coffee = require('gulp-coffee')
+    debug = require('gulp-debug');
 
-gulp.task('log', function() {
-  gutil.log('This is just a log of ultimate destiny');
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(debug())
+    .pipe(coffee({bare:true})
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'));
+  gutil.log('Coffee files added to scripts folder');
 });

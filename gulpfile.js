d75c013a27d401ab851f367e323f054cc1dde934
Robert Kovacs <robert.kovacs.centacc@gmail.com>
2017-07-30 08:55:51 +0200

    added live reload gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util')
-    coffee = require('gulp-coffee')
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
     debug = require('gulp-debug'),
     concat = require('gulp-concat'),
     browserify = require('gulp-browserify'),
-    compass = require('gulp-compass');
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect');
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = [
@@ -14,6 +15,13 @@ var jsSources = [
   'components/scripts/template.js'
 ];
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+
+gulp.task('html', function() {
+  gulp.src(htmlSources)
+    .pipe(debug())
+    .pipe(connect.reload());
+});
 
 gulp.task('coffee', function() {
   gulp.src(coffeeSources)
@@ -28,7 +36,8 @@ gulp.task('js', function() {
     .pipe(debug())
     .pipe(concat('script.js'))
     .pipe(browserify())
-    .pipe(gulp.dest('builds/development/js'));
+    .pipe(gulp.dest('builds/development/js'))
+    .pipe(connect.reload());
 });
 
 gulp.task('sass', function() {
@@ -39,15 +48,25 @@ gulp.task('sass', function() {
       images: 'builds/development/images',
       style: 'expanded'
     }).on('error', gutil.log))
-    .pipe(gulp.dest('builds/development/css'));
+    .pipe(gulp.dest('builds/development/css'))
+    .pipe(connect.reload());
 });
 
-gulp.task('build', ['coffee', 'js', 'sass']);
-
-gulp.task('default', ['build']);
+gulp.task('connect', function() {
+  connect.server({
+    root: 'builds/development',
+    livereload: true,
+    debug: true
+  });
+});
 
 gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
-  gulp.watch('components/sass/*.scss', ['sass']);
+  gulp.watch(['components/sass/*.scss'], ['sass']);
+  gulp.watch(htmlSources, ['html']);
 });
+
+gulp.task('build', ['connect', 'coffee', 'js', 'sass', 'watch']);
+
+gulp.task('default', ['build']);
